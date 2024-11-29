c143cbfbc54495d4bf1f5dc703de1ec2c4cd071b
yashika0977 <123066167+yashika0977@users.noreply.github.com>
2024-11-29 12:21:57 +0530

    Delete models/courses.js

--- models/courses.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose")
-
-const coursesSchema=new mongoose.Schema({
-    name:{type:String , required:true},
-    description:{ type:String},
-    duration:{type:Number, required:true}
-    
-})
-module.exports=mongoose.model("courses", coursesSchema)
